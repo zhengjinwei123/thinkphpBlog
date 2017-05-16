f098b3ae5a15bd4ac65c2b17bdce695ae94d9b4f
zhengjinwei123 <2538698032@qq.com>
2017-05-16 16:52:31 +0800

    update

--- thinkphp_5.0.5_full/app/index/controller/Index.php
+++ thinkphp_5.0.5_full/app/index/controller/Index.php
@@ -152,6 +152,10 @@ class Index extends Controller
 
     public function view2()
     {
+        $this->assign("time", time());
+        $this->assign("list",[
+            1,2,3,4,5,6
+        ]);
         return view();
     }
 
